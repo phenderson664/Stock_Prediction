970cf1b20c20760be7262688491489fa6a3ca64f
phenderson664 <Paris.lmh2@gmail.com>
2026-02-19 17:08:58 -0600

    Update StreamlitApp_Week_3.py

--- Portfolio/StreamlitApp_Week_3.py
+++ Portfolio/StreamlitApp_Week_3.py
@@ -59,8 +59,8 @@ MODEL_INFO = {
         "endpoint": aws_endpoint,
         "explainer": 'explainer.shap',
         "pipeline": 'finalized_model.tar.gz',
-        "keys": ["MSI", "AXON", "LHX", "CSCO", "NOK"],
-        "inputs": [{"name": k, "type": "number", "min": -1.0, "max": 1.0, "default": 0.0, "step": 0.01} for k in ["MSI", "AXON", "LHX", "CSCO", "NOK"]]
+        "keys": ["AXON", "LHX", "CSCO", "NOK"],
+        "inputs": [{"name": k, "type": "number", "min": -1.0, "max": 1.0, "default": 0.0, "step": 0.01} for k in ["AXON", "LHX", "CSCO", "NOK"]]
 }
 
 def load_pipeline(_session, bucket, key):
@@ -151,6 +151,7 @@ if submitted:
         display_explanation(input_df,session, aws_bucket)
     else:
         st.error(res)
+
 
 
 
