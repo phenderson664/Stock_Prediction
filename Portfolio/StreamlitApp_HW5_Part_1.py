e7ce356f21036f1504bc80c15334f66ea99fb8e6
phenderson664 <Paris.lmh2@gmail.com>
2026-04-09 12:06:17 -0500

    Update StreamlitApp_HW5_Part_1.py

--- Portfolio/StreamlitApp_HW5_Part_1.py
+++ Portfolio/StreamlitApp_HW5_Part_1.py
@@ -57,8 +57,8 @@ MODEL_INFO = {
         "endpoint": aws_endpoint,
         "explainer": 'explainer_pca.shap', 
         "pipeline": 'finalized_pca_model.tar.gz', 
-        "keys": ["IBM_CR_Cum","NVDA_CR_Cum"], 
-        "inputs": [{"name": k, "type": "number", "min": -100.0, "max": 100.0, "default": 0.0, "step": 10.0} for k in ["IBM_CR_Cum","NVDA_CR_Cum"]] 
+        "keys": ["AOS_CR_Cum","ABT_CR_Cum"], 
+        "inputs": [{"name": k, "type": "number", "min": -100.0, "max": 100.0, "default": 0.0, "step": 10.0} for k in ["AOS_CR_Cum","ABT_CR_Cum"]] 
 }
 
 def load_pipeline(_session, bucket, key):
